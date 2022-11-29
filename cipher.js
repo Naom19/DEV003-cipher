13b451adddead62626e43992b55a4509e0dd844a
Naomi Peña <naosan25@protonmail.com>
2022-11-29 16:22:26 -0600

    Updates

---
+++ cipher.js
@@ -0,0 +1,20 @@
+const cipher = {
+  
+  cipher.encode(offset, message) {
+    console.log('entre funcion cipher')
+    let result= "";
+    offset= parseInt(offset);
+    let upperLetter =message.toUpperCase();
+
+    for (let i=0; i < upperLetter.lenght; i++) {
+      let messageAscci= upperLetter.charCodeAt(i);
+      let newText= ((messageAscci - 65 + offset) %26) + 65;
+      result += String.fromCharCode(newText);
+    }
+    return result;
+  }
+  // ...
+
+};
+
+export default cipher;
