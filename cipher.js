52f98b2f3614232d001b8c5359b4fb8ecd9069c8
Naomi Peña <naosan25@protonmail.com>
2022-11-30 19:28:59 -0600

    Updates

--- cipher.js
+++ cipher.js
@@ -1,20 +1,41 @@
-const cipher = {
+//Comentarios en JS
 
-  cipher.encode(offset, message) {
-    console.log('entre funcion cipher')
-    let result= "";
-    offset= parseInt(offset);
-    let upperLetter =message.toUpperCase();
-
-    for (let i=0; i < upperLetter.lenght; i++) {
-      let messageAscii= upperLetter.charCodeAt(i);
-      let newText= ((messageAscii - 65 + offset) %26) + 65;
-      result += String.fromCharCode(newText);
-    }
-    return result;
+// declaramos una funcion en javascript con la palabra reservada 'function' continuado del nombre de la función
+// y esta puede o no tener parámetros. Para este caso no es necesario hacer uso de parámetros ya que obtendremos
+// los valores a partir de 'document.getElementById(IdDeMiElementoHTML).value
+function cipher() {
+  // guardando en la variable message el valor del elemento message, en este caso será el mensaje a cifrar
+  const message = document.getElementById("message").value;
+  // guardando en la variable offset el valor del elemento offset, este nos ayudara a determinar cuantos
+  // caracteres se van a recorrer en nuestro cifrado
+  let offset = document.getElementById("offset").value;
+  // como el valor obtenido no es de tipo numerico, tenemos que transformarlo utilizando la función
+  // parsetInt()
+  offset = parseInt(offset);
+  let result= "";
+  let upperLetter = message.toUpperCase();
+  // aqui voy a recorrer cada uno de los caracteres de mi mensaje
+  // y por cada uno voy a obtener su valor en ASCII
+  for (let i=0; i < upperLetter.length; i++) {
+    let messageAscii = upperLetter.charCodeAt(0);
+    console.log('messageAscii: ' + messageAscii);
+    let newText = ((messageAscii - 65 + offset) % 26) + 65;
+    console.log('newText: ' + newText);
+    result += String.fromCharCode(newText);
   }
-  // ...
+  //imprimimos en consola el mensaje ya encriptado
+  console.log(result);
 
-};
+}
 
-export default cipher;
+
+// const cipher= {
+//   let encryptMessage= document.getElementById("message").addEventListener("click", function(){ 
+//  alert("Encrypting...Encriptando..."")
+//  })
+//       
+//  
+//  }
+//}
+
+// export default cipher;
